e9bae4e69eee3ef46adea67963cddb11cb0fe596
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2023-01-29 12:36:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ const App = () => {
   
   return (
     <div id="main">
-
+   <h1 id="marco-polo">{marcoPolo}</h1>
+      <button id="marco-polo-toggler" onClick={toggleMarcoPolo}>{marcoPoloToggler}</button>
     </div>
   )
 }
