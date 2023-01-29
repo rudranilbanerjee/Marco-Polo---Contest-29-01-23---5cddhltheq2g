9b60408dcd3c02112b68c3ed267f9e6f6727210b
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2023-01-29 12:39:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
-  
+
+import React, {useState} from 'react';
+
+function MarcoPolo(){
+  const [marcoPolo, setMarcoPolo] = useState("Marco");
+  const [marcoPoloToggler, setMarcoPoloToggler] = useState("Polo");
+
+  const toggleMarcoPolo = () => {
+    marcoPolo === "Marco" ? setMarcoPolo("Polo") : setMarcoPolo("Marco");
+    marcoPoloToggler === "Polo" ? setMarcoPoloToggler("Marco") : setMarcoPoloToggler("Polo");
+  };
+
   return (
-    <div id="main">
-   <h1 id="marco-polo">{marcoPolo}</h1>
+    <div>
+      <h1 id="marco-polo">{marcoPolo}</h1>
       <button id="marco-polo-toggler" onClick={toggleMarcoPolo}>{marcoPoloToggler}</button>
     </div>
-  )
+  );
 }
 
-
-export default App;
+export default MarcoPolo;
